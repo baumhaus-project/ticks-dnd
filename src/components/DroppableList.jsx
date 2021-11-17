03c5d1174083291b370642c9bd162b2a8ae13ed0
Bastian Nispel <71786901+bastiannispel@users.noreply.github.com>
2021-11-17 19:38:15 +0100

    Multi Column DnD (#2)
    * styled with custom color palette
    
    * multi dnd stable

--- src/components/DroppableList.jsx
+++ src/components/DroppableList.jsx
@@ -1,39 +1,46 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { PropTypes } from 'prop-types';
 import { v4 as uuidv4 } from 'uuid';
+import { PropTypes } from 'prop-types';
 import { Droppable } from 'react-beautiful-dnd';
 
 import DraggableCard from './DraggableCard';
 import './style.css';
 
-export default function DroppableList({ items }) {
+export default function DroppableList({ column }) {
   return (
-    <Droppable droppableId={uuidv4()}>
+    <Droppable droppableId={column.id}>
       {(provided) => (
-        <ul
-          className="list"
-          ref={provided.innerRef}
-          {...provided.droppableProps}
-        >
-          {items.map((item, index) => (
-            <DraggableCard item={item} index={index} key={uuidv4()} />
-          ))}
-          {provided.placeholder}
-        </ul>
+        <div className="column">
+          <h2 className="column-title">{column.title}</h2>
+          <ul
+            className="list"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {column.cards.map((item, index) => (
+              <DraggableCard item={item} index={index} key={uuidv4()} />
+            ))}
+            {provided.placeholder}
+          </ul>
+        </div>
       )}
     </Droppable>
   );
 }
 
 DroppableList.propTypes = {
-  items: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      title: PropTypes.string.isRequired,
-      customer: PropTypes.string.isRequired,
-      assignees: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
+  column: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    cards: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        customer: PropTypes.string.isRequired,
+        assignees: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+      }).isRequired,
+    ).isRequired,
+  }).isRequired,
 };
--- src/layouts/Kanban.jsx
+++ src/layouts/Kanban.jsx
@@ -3,52 +3,115 @@ import { v4 as uuidv4 } from 'uuid';
 import { DragDropContext } from 'react-beautiful-dnd';
 
 import DroppableList from '../components/DroppableList';
+import './style.css';
 
 function item({ title, customer, assignees, description }) {
   return { id: uuidv4(), title, customer, assignees, description };
 }
 
-const data = [
-  item({
-    title: 'CardA',
-    customer: 'CustomerA',
-    assignees: 'EmployerA',
-    description: 'This is the description of CardA',
+function column({ title, cards }) {
+  return { id: uuidv4(), title, cards };
+}
+
+const columns = [
+  column({
+    title: 'Mailbox',
+    cards: [
+      item({
+        title: 'CardA',
+        customer: 'CustomerA',
+        assignees: 'EmployerA',
+        description: 'This is the description of CardA',
+      }),
+      item({
+        title: 'CardB',
+        customer: 'CustomerA',
+        assignees: 'EmployerA',
+        description: 'This is the description of CardB',
+      }),
+      item({
+        title: 'CardC',
+        customer: 'CustomerB',
+        assignees: 'EmployerC',
+        description: 'This is the description of CardC',
+      }),
+    ],
   }),
-  item({
-    title: 'CardB',
-    customer: 'CustomerA',
-    assignees: 'EmployerA',
-    description: 'This is the description of CardB',
+  column({
+    title: 'In Progress',
+    cards: [
+      item({
+        title: 'CardD',
+        customer: 'CustomerC',
+        assignees: 'EmployerB',
+        description: 'This is the description of CardD',
+      }),
+    ],
   }),
-  item({
-    title: 'CardC',
-    customer: 'CustomerB',
-    assignees: 'EmployerC',
-    description: 'This is the description of CardC',
-  }),
-  item({
-    title: 'CardD',
-    customer: 'CustomerC',
-    assignees: 'EmployerB',
-    description: 'This is the description of CardD',
-  }),
+  column({ title: 'Done', cards: [] }),
 ];
 
 export default function Kanban() {
-  const [cards, updateCards] = useState(data);
+  const [data, updateData] = useState(columns);
 
   const handleOnDragEnd = (result) => {
+    const { source, destination } = result;
+
+    // return if destination of drag is outside the context
     if (!result.destination) return;
-    const items = Array.from(cards);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
-    updateCards(items);
+
+    // index of destination object within array
+    const index = data.findIndex((x) => x.id === destination.droppableId);
+    // duplicate array
+    const items = Array.from(data[index].cards);
+
+    // Sorting when the destination is the same arrays
+    if (source.droppableId === destination.droppableId) {
+      // reorder cards with source index
+      const [reorderedItem] = items.splice(source.index, 1);
+      items.splice(destination.index, 0, reorderedItem);
+
+      // update state
+      updateData((prev) => [
+        ...prev.slice(0, index),
+        { ...prev[index], cards: items },
+        ...prev.slice(index + 1),
+      ]);
+    }
+
+    // Movement from one array to another
+    else {
+      // index of source object within array
+      const srcIndex = data.findIndex((x) => x.id === source.droppableId);
+      // duplicate array
+      const srcItems = Array.from(data[srcIndex].cards);
+      const card = srcItems.find((x) => x.id === result.draggableId);
+
+      // insert new card with destination index
+      items.splice(destination.index, 0, card);
+      // remove item from srcItems
+      srcItems.splice(source.index, 1);
+
+      // update state
+      const firstIndex = index > srcIndex ? srcIndex : index;
+      const secondIndex = index > srcIndex ? index : srcIndex;
+      updateData((prev) => [
+        ...prev.slice(0, firstIndex),
+        { ...prev[firstIndex], cards: index > srcIndex ? srcItems : items },
+        ...prev.slice(firstIndex + 1, secondIndex),
+        { ...prev[secondIndex], cards: index > srcIndex ? items : srcItems },
+        ...prev.slice(secondIndex + 1),
+      ]);
+    }
   };
 
   return (
-    <DragDropContext onDragEnd={handleOnDragEnd}>
-      <DroppableList items={cards} />
-    </DragDropContext>
+    <div className="Kanban">
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        {data.map((col) => (
+          <DroppableList key={uuidv4()} column={col} />
+        ))}
+      </DragDropContext>
+    </div>
   );
 }
