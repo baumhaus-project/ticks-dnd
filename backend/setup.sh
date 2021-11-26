c0ad46d73f8079f065a31c39d51abe2509fd8949
Bastian Nispel <b.nispel@t-online.de>
2021-11-26 14:39:47 +0100

    added ./start.sh and modified ./setup.sh

--- backend/setup.sh
+++ backend/setup.sh
@@ -6,7 +6,8 @@ then
   pip install -r requirements.txt
   cd ./ticks || exit
   python manage.py makemigrations
-  python manage.py migrate
+  python manage.py makemigrations api
+  python manage.py migrate  
   cd .. || exit
   deactivate
 fi
---
+++ backend/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [ ! -d ".env" ]
+then
+  source ./setup.sh
+fi
+
+source .env/bin/activate
+cd ./ticks || exit
+python manage.py runserver
